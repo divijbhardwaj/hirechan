d0ddb586ef5b0486b0ea781c4286c150e56dfeea
divij <divij.827@gmail.com>
2023-05-28 03:17:02 -0400

    refactoring code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FirebaseAppProvider } from 'reactfire';
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import firebaseConfig from '../firebase-config'
 import IndexPage from './routes/Index.tsx'
 
 import '@fontsource/roboto/300.css';
@@ -11,6 +10,16 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './main.scss'
 
+const firebaseConfig =  {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+console.log(firebaseConfig)
 const router = createBrowserRouter([
   {
     path: "/",
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,9 +1,30 @@
-import Button from '@mui/material/Button';
+import { doc, getFirestore } from 'firebase/firestore';
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
 
-export default function Index() {
+function BurritoTaste() {
+  // easily access the Firestore library
+  const burritoRef = doc(useFirestore(), 'tryreactfire', 'burrito');
+
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData(burritoRef);
+
+  // easily check the loading status
+  if (status === 'loading') {
+    return <p>Fetching burrito flavor...</p>;
+  }
+
+  return <p>The burrito is {data.yummy ? 'good' : 'bad'}!</p>;
+}
+
+function App() {
+  const firestoreInstance = getFirestore(useFirebaseApp());
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <FirestoreProvider sdk={firestoreInstance}>
+      {/* <h1>🌯</h1>
+      <BurritoTaste /> */}
+      <h1>The site is under construction</h1>
+    </FirestoreProvider>
   );
 }
+
+export default App
