c7ef4ced4645d6ad653e2d24991a41867bff3c50
divij <divij.827@gmail.com>
2023-05-28 03:26:50 -0400

    wip

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,30 +1,7 @@
-import { doc, getFirestore } from 'firebase/firestore';
-import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
-
-function BurritoTaste() {
-  // easily access the Firestore library
-  const burritoRef = doc(useFirestore(), 'tryreactfire', 'burrito');
-
-  // subscribe to a document for realtime updates. just one line!
-  const { status, data } = useFirestoreDocData(burritoRef);
-
-  // easily check the loading status
-  if (status === 'loading') {
-    return <p>Fetching burrito flavor...</p>;
-  }
-
-  return <p>The burrito is {data.yummy ? 'good' : 'bad'}!</p>;
-}
-
-function App() {
-  const firestoreInstance = getFirestore(useFirebaseApp());
+function Index() {
   return (
-    <FirestoreProvider sdk={firestoreInstance}>
-      {/* <h1>🌯</h1>
-      <BurritoTaste /> */}
       <h1>The site is under maintenance</h1>
-    </FirestoreProvider>
   );
 }
 
-export default App
+export default Index
