2651f492a3d61c86976f64a2657e1f03155b7831
divij <divij.827@gmail.com>
2023-05-27 21:45:43 -0400

    react router added

---
+++ firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyD24EM2CK-gfjIygIiOfpZevl94QUpcnJU",
+  authDomain: "hiring-platform-559b3.firebaseapp.com",
+  projectId: "hiring-platform-559b3",
+  storageBucket: "hiring-platform-559b3.appspot.com",
+  messagingSenderId: "613866080867",
+  appId: "1:613866080867:web:c886ebade5fbbd2a460656",
+  measurementId: "G-73CP5V69K9"
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { FirebaseAppProvider } from 'reactfire';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import firebaseConfig from '../firebase-config'
+import App from './routes/App.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <RouterProvider router={router} />
+    </FirebaseAppProvider>
   </React.StrictMode>,
 )
